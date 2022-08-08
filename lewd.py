52436c3ebe37e4e3bae4a8aad54a5d49510c76c6
rezuwii <59052194+rezuwii@users.noreply.github.com>
2022-08-08 21:16:24 +0530

    added member while loop

--- lewd.py
+++ lewd.py
@@ -28,18 +28,18 @@ async def supply(ctx):
     i = 0
     while i<=10: 
         members = [852797584812670996, 409994220309577729]
-        mem1 = ctx.guild.get_member(members[0])
-        mem2 = ctx.guild.get_member(members[1])
-        url = "https://api.waifu.im/random/?" \
-        "&gif=false" \
-        "&is_nsfw=true"
-        async with aiohttp.ClientSession() as session:
-            async with session.get(url) as response:
-                res = await response.json()
-                await mem1.send(f"|| {res['images'][0]['url']} ||")
-                await mem2.send(f"|| {res['images'][0]['url']} ||") 
+        z = 0
+        while z <= members.len(): 
+            mem = ctx.guild.get_member(members[z])
+            url = "https://api.waifu.im/random/?" \
+            "&gif=false" \
+            "&is_nsfw=true"
+            async with aiohttp.ClientSession() as session:
+                async with session.get(url) as response:
+                    res = await response.json()
+                    await mem.send(f"|| {res['images'][0]['url']} ||")
 
-        await asyncio.sleep(43200)
+            await asyncio.sleep(43200)
 
 
 asyncio.run(main())
